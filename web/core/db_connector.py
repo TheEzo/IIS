9f2801216e9a87d4d249bcd0a5ed5faeade8950e
nxf39066 <tomas.willaschek@nxp.com>
2018-09-30 11:37:29 +0200

    db script

--- web/core/db_connector.py
+++ web/core/db_connector.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('mysql://root@localhost/iis?charset=utf8',
+engine = create_engine('mysql://xwilla00:acirem2a@eva.fit.vutbr.cz/xwilla00?charset=utf8',
                        pool_recycle=3600)
 session = scoped_session(sessionmaker(autocommit=False,
                                       autoflush=True,
