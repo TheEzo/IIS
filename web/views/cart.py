1ba6ff4c8ab609aa01ecab674f30aa49d189d50b
TheEzo <willaschek.t@gmail.com>
2020-04-12 15:01:58 +0200

    deleted redundant

--- web/views/cart.py
+++ web/views/cart.py
@@ -9,82 +9,105 @@ from datetime import timedelta
 
 from web.roles import login_required
 from web.core import db
+from web.views.costumes import Costumes
+from web.views.accessories import Accessories
+
+
+# class Cart(MethodView):
+#     def actualizePrize(self, action, type, id):
+#         if(action=="add"):
+#             session['cart']['prize'] += db.get_prize(type,id)[0]
+#         elif(action=="remove"):
+#             session['cart']['prize'] -= db.get_prize(type, id)[0]
+#             if (session['cart']['prize'] < 0):
+#                 session['cart']['prize'] = 0
+#
+#     def actualizeAmount(self,action,type,id):
+#         if (action == "add"):
+#             amount = db.get_product_amount(type, id)[0]
+#             db.set_product_amount(type,id,amount-1)
+#         elif (action == "remove"):
+#             amount = db.get_product_amount(type, id)[0]
+#             db.set_product_amount(type, id, amount + 1)
+#
+#     @login_required
+#     def get(self):
+#         items = []
+#         price = 0
+#         for id in session['cart']['costumes']:
+#             data = db.get_product(id, "costumes")
+#             price += data.cena
+#             items.append(data)
+#
+#         for id in session['cart']['accessories']:
+#             data = db.get_product(id, "accessories")
+#             items.append(data)
+#             price += data.cena
+#         session['cart']['prize'] = price
+#
+#         return render_template('cart.html', items=list(set(items)), cos_count=Counter(session['cart']['costumes']),
+#                                acc_count=Counter(session['cart']['accessories']), price=price)
+#
+#     def post(self):
+#         if not current_user.is_authenticated():
+#             flash('Pro tuto akci je vyžadováno přihlášení. <a href="' + url_for('login') + '">Přihlásit?</a>', "alert-warning")
+#             return jsonify({})
+#         data = request.json
+#         if data['action'] == 'add':
+#             db_data = db.get_product_amount(data['type'], data['value'])[0]
+#             if db_data == 0:
+#                 flash('Toto zboží je momentálně nedostupné', 'alert-danger')
+#                 return jsonify({})
+#             if session['cart'][data['type']].count(data['value']) == db_data:
+#                 order = db.get_costume_order(data['value'])
+#                 if order:
+#                     flash("Položku není aktuálně možná vložit do košíku, dostupná bude od %s" %
+#                           (order.Vypujcka.datum_vraceni + timedelta(days=1)).strftime('%d.%m.%Y'), "alert-danger")
+#                 else:
+#                     flash('Nelze vložit více těchto položek', 'alert-danger')
+#                 return jsonify({})
+#
+#             if data['type'] == 'costumes':
+#                 session['cart']['costumes'].append(data['value'])
+#             elif data['type'] == 'accessories':
+#                 session['cart']['accessories'].append(data['value'])
+#             else:
+#                 flash('Něco se pokazilo', 'alert-danger')
+#                 return jsonify({})
+#             flash("Položka byla přidána do košíku", 'alert-success')
+#
+#         elif data['action'] == 'remove':
+#             if data['type'] == 'costumes':
+#                     session['cart']['costumes'].remove(data['value'])
+#             elif data['type'] == 'accessories':
+#                     session['cart']['accessories'].remove(data['value'])
+#             else:
+#                 flash('Něco se pokazilo', 'alert-danger')
+#                 return jsonify({})
+#             flash("Položka byla odebrána z košíku", 'alert-success')
+#         return jsonify({})
 
 class Cart(MethodView):
-    def actualizePrize(self,action,type,id):
-        if(action=="add"):
-            session['cart']['prize'] += db.get_prize(type,id)[0]
-        elif(action=="remove"):
-            session['cart']['prize'] -= db.get_prize(type, id)[0]
-            if (session['cart']['prize'] < 0):
-                session['cart']['prize'] = 0
-
-    def actualizeAmount(self,action,type,id):
-        if (action == "add"):
-            amount = db.get_product_amount(type, id)[0]
-            db.set_product_amount(type,id,amount-1)
-        elif (action == "remove"):
-            amount = db.get_product_amount(type, id)[0]
-            db.set_product_amount(type, id, amount + 1)
-
-    @login_required
+    # @login_required
     def get(self):
-        items = []
-        price = 0
-        for id in session['cart']['costumes']:
-            data = db.get_product(id, "costumes")
-            price += data.cena
-            items.append(data)
-
-        for id in session['cart']['accessories']:
-            data = db.get_product(id, "accessories")
-            items.append(data)
-            price += data.cena
-        session['cart']['prize'] = price
-
-        return render_template('cart.html', items=list(set(items)), cos_count=Counter(session['cart']['costumes']),
-                               acc_count=Counter(session['cart']['accessories']), price=price)
+        session['cart'] = {}
+        session['cart']['accessories'] = [17, 9, 8, 8]
+        session['cart']['costumes'] = [31, 33, 44]
+        items = dict(costumes=[], accessories=[])
+        for costume in session.get('cart', {}).get('costumes', []):
+            items['costumes'].append(Costumes.data_json(db.get_costume_by_id(costume)))
+        for accessory in session.get('cart', {}).get('accessories', []):
+            items['accessories'].append(Accessories.data_json(db.get_accessory_by_id(accessory)))
+        items['price'] = sum([i['price'] for i in items['costumes'] + items['accessories']])
+        return jsonify(items)
 
     def post(self):
-        if not current_user.is_authenticated():
-            flash('Pro tuto akci je vyžadováno přihlášení. <a href="' + url_for('login') + '">Přihlásit?</a>', "alert-warning")
-            return jsonify({})
-        data = request.json
-        if data['action'] == 'add':
-            db_data = db.get_product_amount(data['type'], data['value'])[0]
-            if db_data == 0:
-                flash('Toto zboží je momentálně nedostupné', 'alert-danger')
-                return jsonify({})
-            if session['cart'][data['type']].count(data['value']) == db_data:
-                order = db.get_costume_order(data['value'])
-                if order:
-                    flash("Položku není aktuálně možná vložit do košíku, dostupná bude od %s" %
-                          (order.Vypujcka.datum_vraceni + timedelta(days=1)).strftime('%d.%m.%Y'), "alert-danger")
-                else:
-                    flash('Nelze vložit více těchto položek', 'alert-danger')
-                return jsonify({})
+        # zkontrolovat pocet v db >0
+        ...
 
-            if data['type'] == 'costumes':
-                session['cart']['costumes'].append(data['value'])
-            elif data['type'] == 'accessories':
-                session['cart']['accessories'].append(data['value'])
-            else:
-                flash('Něco se pokazilo', 'alert-danger')
-                return jsonify({})
-            flash("Položka byla přidána do košíku", 'alert-success')
-
-        elif data['action'] == 'remove':
-            if data['type'] == 'costumes':
-                    session['cart']['costumes'].remove(data['value'])
-            elif data['type'] == 'accessories':
-                    session['cart']['accessories'].remove(data['value'])
-            else:
-                flash('Něco se pokazilo', 'alert-danger')
-                return jsonify({})
-            flash("Položka byla odebrána z košíku", 'alert-success')
-        return jsonify({})
+    def delete(self):
+        ...
 
 
 def configure(app):
-    app.add_url_rule('/cart',
-                 view_func=Cart.as_view('cart'))
+    app.add_url_rule('/cart', view_func=Cart.as_view('cart'))
