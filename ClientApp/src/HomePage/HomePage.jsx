2b4b536362f95b1ff0f85f7f12abdb1c27114736
Šimon Pokorný <salamon.sima@gmail.com>
2020-04-12 23:33:10 +0200

    Edited home page

--- ClientApp/src/HomePage/HomePage.jsx
+++ ClientApp/src/HomePage/HomePage.jsx
@@ -5,7 +5,9 @@ class HomePage extends React.Component {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h1>Vítejte v půjčovně kostýmů</h1>
-                <h2>Titulní stránka</h2>
+                <h2>Autoři:</h2>
+                <h3>Bc. Šimon Pokorný</h3>
+                <h3>Bc. Tomáš Willaschek</h3>
             </div>
         );
     }
